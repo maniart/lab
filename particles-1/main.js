b372515420ce863630beea082e998d1da975c078
Mani Nilchiani <mani.art@gmail.com>
2013-09-10 08:25:01 -0400

    Starting new

--- particles-1/main.js
+++ particles-1/main.js
@@ -1,88 +1,73 @@
-// RequestAnimFrame: a browser API for getting smooth animations
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       || 
-		  window.webkitRequestAnimationFrame || 
-		  window.mozRequestAnimationFrame    || 
-		  window.oRequestAnimationFrame      || 
-		  window.msRequestAnimationFrame     ||  
-		  function( callback ){
-			window.setTimeout(callback, 1000 / 60);
-		  };
-})();
-
-var app = (function(w, d, undefined) {
+;(function(w) {
 
 	'use strict';
-	// private
-	var prv = {};
-		// constants
-		prv.CONST = {};
-		prv.CONST.W = w.innerWidth;
-		prv.CONST.H = w.innerHeight;
-		// vars
-		prv.particles = {};
-		prv.canvas = d.querySelector('#canvas');
-		prv.ctx = prv.canvas.getContext('2d');
-		prv.particleCount = 150;
-		prv.minDist = 70;
-		prv.dist = null;
-		prv.counter = 0;
-		// functions
-		prv.paintCanvas = function() {
-			prv.ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-			prv.ctx.fillRect(0, 0, 1434, 591);
+	console.log('Starting from scratch');
+
+	var canvas = document.getElementById('canvas'),
+		ctx = canvas.getContext('2d'),
+		W = w.innerWidth,
+		H = w.innerHeight,
+		numParticles = 150,
+		particles = [],
+		Particle = function(x, y) {
+			this.x = Math.random() * W;
+			this.y = Math.random() * H;
+			this.radius = 4;
+
 		};
-		prv.Particle = function() {
-			this.x 		= 	Math.random() * prv.CONST.W;
-			this.y 		= 	Math.random() * prv.CONST.H;
-			this.vx		=	-1 + Math.random() * 2;
-			this.vy		=	-1 + Math.random() * 2;
-			this.radius	=	Math.random() * 4;
-			this.draw	=	function() {
-				ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-				ctx.beginPath();
-				ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-				ctx.fill();
-			};
+		canvas.width = W;
+		canvas.height = H;
+		ctx.fillStyle = "rgba(0,0,0,1)";
+	
+	// This will create a rectangle of white color from the 
+	// top left (0,0) to the bottom right corner (W,H)
+	ctx.fillRect(0,0,W,H);
+		Particle.prototype.draw = function() {
+			ctx.fillStyle = "white";
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+			console.log('drawing myself');
 		};
-		prv.createParticles = function() {
-			for (var i = prv.particleCount - 1; i >= 0; i--) {
-				prv.particles.push(new Particle());
-			};
-		};
-		prv.draw = function() {
-			prv.paintCanvas();
-			for (var i = prv.particles.length - 1; i >= 0; i--) {
-				var p = prv.particles[i];
-				p.x += p.vx;
-				p.y += p.vy;
-				if(p.x + p.radius > prv.CONST.W) {
-					p.x = p.radius;
-				} else if(p.x - p.radius < 0) {
-					p.x = prv.CONST.W - p.radius;
-				};
-				if(p.y + p.radius > prv.CONST.H) {
-					p.y = p.radius;
-				} else if(p.y - p.radius < 0) {
-					p.y = prv.CONST.H - p.radius;
-				};
+
+	function setup() {
+		for (var i = numParticles - 1; i >= 0; i--) {
+			particles.push(new Particle);
 				
-
-			};
 		};
+		console.log(particles);
+		
 
-
-
-	// private
-	var init = function() {
-		console.log(prv);
 	};
 
-	// public
-	return {
-		init : init
+	function draw() {
+		for (var i = particles.length - 1; i >= 0; i--) {
+			particles[i].draw();
+		};
 	};
 
-})(window, document, undefined);
+	function loop() {
+		requestAnimFrame(loop);
+	};
 
-window.onload = app.init
+	function init() {
+		setup();
+		draw();
+		loop();
+	};
+
+
+	// RequestAnimFrame: a browser API for getting smooth animations
+	window.requestAnimFrame = (function(){
+  	return  window.requestAnimationFrame       || 
+		  	window.webkitRequestAnimationFrame || 
+		  	window.mozRequestAnimationFrame    || 
+		  	window.oRequestAnimationFrame      || 
+		  	window.msRequestAnimationFrame     ||  
+		  	function( callback ){
+				window.setTimeout(callback, 1000 / 60);
+		  	};
+	})();
+
+	w.onload = init;
+
+})(window);
