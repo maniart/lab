d16416fe51ad42138288ec57e7e8d7a7ddf0d21c
Mani Nilchiani <mani.art@gmail.com>
2013-09-14 23:55:49 -0400

    ready to launch the labs

--- particles-1/main.js
+++ particles-1/main.js
@@ -2,21 +2,48 @@
 
 	'use strict';
 
-		// Vars
+	// Vars
 	var canvas = document.getElementById('canvas'),
 		ctx = canvas.getContext('2d'),
 		W = w.innerWidth,
 		H = w.innerHeight,
-		numParticles = 140,
-		minDist = 100,
-		particles = [],
+		particles = [],	
 		dist,
+		args = {
+			radius : 1,
+			minDist : 100,
+			blur : 1,
+			colors: [
+				'rgba(100,200,300)',
+				'rgba(200,0,22)',
+				'rgba(255,23,200)'
+			],
+
+			isColor : false,
+			toggleColor: function() {
+				console.log(args.isColor);
+				args.isColor != args.isColor;
+
+				if (this.isColor) {
+					for (var i = particles.length ; i > 0 ; i -= 1) {
+						return colors[ Math.floor(Math.random() * 3) ];	
+					}
+				} else {
+					for (var i = particles.length ; i > 0 ; i -= 1) {
+						return 'rgba(0,0,0,1)';	
+					}
+				}
+
+			} 
+		},
 		// Particle constructor
-		Particle = function() {
+		Particle = function(args) {
 			this.x = Math.random() * W;
 			this.y = Math.random() * H;
+			this.color = 'rgba(0,0,0,1)';
 			this.vx = -1 + Math.random() * 2;
 			this.vy = -1 + Math.random() * 2;
+			this.radius = args.radius;
 		};
 	// Setting up the stage
 	canvas.width = W;
@@ -24,57 +51,56 @@
 	ctx.fillStyle = "rgba(255,255,255,1)";
 	ctx.fillRect(0, 0, W, H);
 	// Shared members
+	//Particle.prototype.radius = 1;
 	Particle.prototype.draw = function() {
-		ctx.fillStyle = "black";
+		ctx.fillStyle = this.color;
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 		ctx.fill();
-	};
-	Particle.prototype.radius = 1;
-	
+	};	
 	function distance(p1, p2) {
 		var dx = p1.x - p2.x,
 			dy = p1.y - p2.y;
 		dist = Math.sqrt(dx * dx + dy * dy);
-		if (dist <= minDist) {
+		if (dist <= args.minDist) {
 			
 			ctx.beginPath();
-			ctx.strokeStyle = "rgba(0,0,0,"+ (.4-dist/minDist) +")";
+			ctx.strokeStyle = "rgba(0,0,0,"+ (.4-dist/args.minDist) +")";
 
 			ctx.moveTo(p1.x, p1.y);
 			ctx.lineTo(p2.x, p2.y);
 			ctx.stroke();
 			
 			ctx.closePath();
-			var ax = dx / 2000,
-				ay = dy / 2000;
-			p1.vx -= .2 * ax;
-			p1.vy -= .2 * ay;	
-			p2.vx += .2 * ax;
-			p2.vy += .2 * ay;
+			var ax = dx / (args.minDist * 20),
+				ay = dy / (args.minDist * 20);
+			p1.vx -= (args.minDist / Math.pow(100,2)) * ax;
+			p1.vy -= (args.minDist / Math.pow(100,2)) * ay;	
+			p2.vx += (args.minDist / Math.pow(100,2)) * ax;
+			p2.vy += (args.minDist / Math.pow(100,2)) * ay;
 		}	
 	};
 
 	function clear() {
-		ctx.fillStyle = "rgba(255,255,255,.3)"
+		ctx.fillStyle = "rgba(255,255,255,"+ 1/args.blur +")";
 		ctx.fillRect(0, 0, W, H);
 	};
-	function setup() {
-		for (var i = numParticles; i >= 0; i--) {
-			particles.push(new Particle());
-				
+	function setup(args) {
+		for (var i = 200; i >= 0; i--) {
+			var p = new Particle(args);
+			particles.push(p);
 		};
-		//console.log(particles);
+		setupDatGui();
 	};
 
 
 	function update() {
 		var particle1,
 			particle2;
-
+		
 		for (var i = 0; i < particles.length; i ++) {
 			particle1 = particles[i]; 
-			
+			particle1.radius = args.radius;
 			particle1.x += particle1.vx;
 			particle1.y += particle1.vy;
 
@@ -88,15 +114,13 @@
 			} else if (particle1.y < 0) {
 				particle1.vy *= -1;
 			}
-			
+
 			for(var j = i + 1; j < particles.length; j++) {
 				particle2 = particles[j];
 				distance(particle1, particle2);
 			}
-
-
-	
 		}
+
 	};
 	function draw() {
 		clear();
@@ -109,12 +133,18 @@
 		draw();
 		requestAnimFrame(loop);
 	};
+	function setupDatGui() {
+	    var gui = new dat.GUI();
+	    gui.remember(args);
+	    gui.add(args, 'radius', 0, 10).step(1);
+	    gui.add(args, 'minDist', 100, 200).step(1);
+		gui.add(args, 'blur', 1, 100).step(1);
+		gui.add(args, 'toggleColor');
+	};
 	function init() {
-		setup();
+		setup(args);
 		loop();
 	};
-
-
 	// RequestAnimFrame: a browser API for getting smooth animations
 	window.requestAnimFrame = (function(){
   	return  window.requestAnimationFrame       || 
